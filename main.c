3fe4944f8a42a5f35dd6c28fa809d0b6a59536db
jessenjavier <81350573+jessenjavier@users.noreply.github.com>
2024-02-23 21:27:13 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+/** EL2208 Praktikum Pemecahan Masalah dengan C 2022/2023
+ *   Modul               : 
+ *   Hari dan Tanggal    :
+ *   Nama (NIM)          :
+ *   Nama File           : main.c
+ *   Deskripsi           :
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Halo dunia!\n");
+    return 0;
+}
